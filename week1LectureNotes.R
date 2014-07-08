24bc3b3d5f7124e5272c59b1d7c4658ac77b6639
Brad Lindaas <brad@lindaas.org>
2014-07-07 22:27:54 -0500

    finished week 1 quiz

--- week1LectureNotes.R
+++ week1LectureNotes.R
@@ -57,3 +57,37 @@ library(ggplot2)
 data(mpg)
 qplot(displ, hwy, data = mpg)
 
+#BASE
+
+#basic
+library(datasets)
+hist(airquality$Ozone) ## Draw a new plot
+
+with(airquality, plot(Wind, Ozone))
+
+airquality <- transform(airquality, Month = factor(Month))
+boxplot(Ozone ~ Month, airquality, xlab = "Month", ylab = "Ozone (ppb)")
+
+with(airquality, plot(Wind, Ozone, main = "Ozone and Wind in New York City", type = "n"))
+with(subset(airquality, Month == 5), points(Wind, Ozone, col = "blue"))
+with(subset(airquality, Month != 5), points(Wind, Ozone, col = "red"))
+legend("topright", pch = 1, col = c("blue", "red"), legend = c("May", "Other Months"))
+
+with(airquality, plot(Wind, Ozone, main = "Ozone and Wind in New York City", pch = 20))
+model <- lm(Ozone ~ Wind, airquality)
+abline(model, lwd = 2)
+
+par(mfrow = c(1, 3), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
+with(airquality, {
+    plot(Wind, Ozone, main = "Ozone and Wind")
+    plot(Solar.R, Ozone, main = "Ozone and Solar Radiation")
+    plot(Temp, Ozone, main = "Ozone and Temperature")
+    mtext("Ozone and Weather in New York City", outer = TRUE)
+})
+
+#Demo of base plot
+x <- rnorm(1000)
+y <- rnorm(1000)
+plot(x,y, pch=20)
+fit <- lm(x~y)
+abline(fit, lwd=3,col="red")
