96b09df1cfdd1efaa6d0e14283422c3529a569cf
Brad Lindaas <brad@lindaas.org>
2014-07-12 11:46:57 -0500

    First project 2 commit

---
+++ project_2.R
@@ -0,0 +1,9 @@
+dataDir <- "/home/rstudio/largedata/"
+dataFileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+if (!file.exists(paste(dataDir, "summarySCC_PM25.rds", sep="")) | !file.exists(paste(dataDir, "Source_Classification_Code.rds", sep=""))) {
+    download.file(dataFileURL, destfile = paste(dataDir, "exdata-data-NEI_data.zip", sep=""), method="curl")
+    unzip(paste(dataDir, "exdata-data-NEI_data.zip", sep=""), exdir=dataDir)
+}
+
+NEI <- readRDS(paste(dataDir, "summarySCC_PM25.rds", sep=""))
+SCC <- readRDS(paste(dataDir, "Source_Classification_Code.rds", sep=""))
